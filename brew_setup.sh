21dcf61e32663d5312a30cc95507579767c91673
Sergey Demchuk <sergey.demchuk@gmail.com>
2017-03-07 10:04:52 +0200

    Update bash-completion formulae

--- brew_setup.sh
+++ brew_setup.sh
@@ -14,15 +14,18 @@ brew install coreutils
 
 # Install some other useful utilities like `sponge`.
 brew install moreutils
+
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
 brew install findutils
+
 # Install GNU `sed`, overwriting the built-in `sed`.
 brew install gnu-sed --with-default-names
+
 # Install Bash 4.
 # Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
 # running `chsh`.
 brew install bash
-brew install homebrew/versions/bash-completion2
+brew install bash-completion@2
 
 # Switch to using brew-installed bash as default shell
 if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
